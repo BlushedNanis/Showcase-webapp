a2f793571679b7ffc10587d24e19e5c9769e0aab
BlushedNanis <luis_danieltg@outlook.com>
2024-05-20 18:06:42 -0400

    Created model

--- showcase/models.py
+++ showcase/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 
+class ProjectsData(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    link = models.URLField(max_length=200)
+    image = models.ImageField(upload_to="showcase\\static\\images\\")
+    
+    def short_description(self):
+        first_words = self.description.split()[:10]
+        short_description = " ".join(first_words)
+        return short_description
+    
+    def img_static_path(self):
+        return self.image[23:]
+    
+    def __str__(self) -> str:
+        return self.title
