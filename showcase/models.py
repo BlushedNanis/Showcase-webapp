4e0089a57a3478205101088f5186c68f30ee1a7a
BlushedNanis <luis_danieltg@outlook.com>
2024-05-21 05:53:33 -0400

    Added dates to projects data model

--- showcase/models.py
+++ showcase/models.py
@@ -6,6 +6,8 @@ class ProjectsData(models.Model):
     description = models.CharField(max_length=2000)
     link = models.URLField(max_length=200)
     image = models.ImageField(upload_to="showcase\\static\\images\\")
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     
     def short_description(self):
         first_words = self.description.split()[:10]
