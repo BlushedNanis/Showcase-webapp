c0858635e9b6376f4786e7e7754b3b079767fec9
BlushedNanis <luis_danieltg@outlook.com>
2024-05-20 20:07:09 -0400

    Initial views setup

---
+++ showcase/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ProjectsList
+
+
+urlpatterns = [
+    path("", ProjectsList.as_view(), name="home")
+]
--- showcase/views.py
+++ showcase/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, FormView, TemplateView
+from .models import ProjectsData
 
-# Create your views here.
+
+class ProjectsList(ListView):
+    model = ProjectsData
+    template_name = "projects_list.html"
+    context_object_name = "projects"
