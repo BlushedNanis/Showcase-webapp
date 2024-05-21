4f05df0a18944624ac02be8eedbf7e4cbf1ec60a
BlushedNanis <luis_danieltg@outlook.com>
2024-05-21 13:50:42 -0400

    Added admin models

--- showcase/admin.py
+++ showcase/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import ProjectsData, ProfileData, ContactForm
 
-# Register your models here.
+
+class ProfileDataAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "updated_date")
+    readonly_fields = ("created_date", "updated_date")
+    
+
+class ProjectsDataAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "updated_date")
+    readonly_fields = ("created_date", "updated_date")
+    list_filter = ("created_date", "updated_date")
+    sortable_by = ("title", "created_date", "updated_date")
+    
+    
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "timestamp", "email_sent",
+                    "email_received")
+    readonly_fields = ("name", "email", "subject", "message", "timestamp",
+                       "email_sent", "email_received")
+    list_filter = ("subject", "email", "email_sent", "email_received")
+    sortable_by = ("created_date", "updated_date", "email_sent", "email_received")
+    
+admin.site.register(ProfileData, ProfileDataAdmin)
+admin.site.register(ProjectsData, ProjectsDataAdmin)
+admin.site.register(ContactForm, ContactFormAdmin)
